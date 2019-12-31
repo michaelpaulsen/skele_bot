0e1832f536a645ed4502c1bdd31459fe41baece0
michaelpaulsen <michael.j.paulsen@gmail.com>
2019-12-31 14:49:10 -0800

    version parody
    made the master more closly parody the switch statement branch

--- chatbot.js
+++ chatbot.js
@@ -9,7 +9,23 @@ const cmds = [
 /**
 *list of commands to use for the help command
 */
-	const tmi = require('tmi.js');
+const discord =  "https://discord.gg/eUCdFs9";
+const YT = "https://www.youtube.com/channel/UCKligdusCYH4FZjJ1CGIMKw;"
+/**
+* set these to be your own YT and discord links (keep null if you don't have eather a YT or a discord) 
+*/
+const tmi = require('tmi.js');
+// Function called when the "dice" command is issued
+function rollDice (s) {
+  const sides = s;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+  console.log(`* chat_bot started on ` + (new Date()).toDateString());
+}
 // Define configuration options
 const opts = {
   identity: {
@@ -44,60 +60,54 @@ function onMessageHandler (target, context, msg, self) {
   const cd = "^";
   const cmd = commandName.split(cd)[1];
   // If the command is known, let's execute it
-  if (commandName.includes(cd + 'dice')) {
-		var max = 6 
-		if(commandName.split(' ').length > 1){
-			max = parseInt(commandName.split(' ')[1]);
-		}
-		console.log(max);
-		const num = rollDice(max);
-		client.say(target, `You rolled a ${num}`);
-		console.log(`* Executed ${cmd} command and rolled a ${num}`);
-  } 
-  else if( commandName === cd + "mcseed" ) {
-  client.say(target, `the seed for my ssp world is "${seed}"`);
-    console.log(`* Executed  command ${cmd} `);
-  }
-  else if( commandName === cd + "discord"){
-	 client.say(target, "My discord is : https://discord.gg/eUCdFs9")
-    console.log(`* Executed  command ${cmd} `);
-  }
-  else if( commandName === cd + "YT"){
-	 console.log(`* Executed  command ${cmd} `);
-	 client.say(target, "My YT is : https://www.youtube.com/channel/UCKligdusCYH4FZjJ1CGIMKw")
-	  
-  }
-  else if( commandName === cd + "help" ) { 
-	let str = ""
-	for(var x = 0; x<cmds.length;x++){
-		str += cd + cmds[x]["title"] + " : "  + cmds[x]['discription'] + ";";
-	}
-	client.say(target, str);
-	console.log(`* Executed  command ${cmd} `);
-  }
-  else if( commandName === cd + "manacolors" ) {
-	  const colors = ['red','green','blue','black','white']
-	  var str = "in magic the gathering the colors of mana are"; 
-	  for(var x = 0; x< colors.length; x++){
-		  str += colors[x];
-		  str += " ";
+ if(commandName[0] == cd){
+	 if (commandName.includes('dice')) {
+			var max = 6 
+			if(commandName.split(' ').length > 1){
+				max = parseInt(commandName.split(' ')[1]);
+			}
+			console.log(max);
+			const num = rollDice(max);
+			client.say(target, `You rolled a ${num}`);
+			console.log(`* Executed ${cmd} command and rolled a ${num}`);
+	  } 
+	  else if( commandName === "mcseed" ) {
+	  client.say(target, `the seed for my ssp world is "${seed}"`);
+		console.log(`* Executed  command ${cmd} `);
 	  }
-	  client.say(target,str);
-  }
-   else if( commandName != undefined) {
-	  console.log(`unkown command used: ` + commandName.split(cd)[1]);
-  }
+	  else if( commandName === "discord"){
+		if(discord !== null) {
+			client.say(target, `My discord is : ${discord}`); 
+		}
+		console.log(`* Executed  command ${cmd} `);
+	  }
+	  else if( commandName === "YT"){
+		console.log(`* Executed  command ${cmd} `);
+		if(YT !== null){
+			client.say(target, `My YT is : ${YT}`);
+		} 
+	  }
+	  else if( commandName === "help" ) { 
+		let str = ""
+		for(var x = 0; x<cmds.length;x++){
+			str += cmds[x]["title"] + " : "  + cmds[x]['discription'] + ";";
+		}
+		client.say(target, str);
+		console.log(`* Executed  command ${cmd} `);
+	  }
+	  else if( commandName === "manacolors" ) {
+		  const colors = ['red','green','blue','black','white']
+		  var str = "in magic the gathering the colors of mana are"; 
+		  for(var x = 0; x< colors.length; x++){
+			  str += colors[x];
+			  str += " ";
+		  }
+		  client.say(target,str);
+	  }
+	   else if( commandName != undefined) {
+		  console.log(`unkown command used: ` + commandName.split(cd)[1]);
+	  }
+ }
   /** possible to change to a switch staitment*/
 }
 
-// Function called when the "dice" command is issued
-function rollDice (s) {
-  const sides = s;
-  return Math.floor(Math.random() * sides) + 1;
-}
-
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-  console.log(`* chat_bot started on ` + (new Date()).toDateString());
-}
